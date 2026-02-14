0ac7683c73b5339a790b42f769681174dd11b625
sorites <jjsorites@gmail.com>
2026-02-14 12:22:48 -0600

    Deployed 96d515d3 with MkDocs version: 1.6.1

--- javascripts/tablesort.js
+++
@@ -1,7 +0,0 @@
-document$.subscribe(function() {
-  // Only target tables with the "sortable" class
-  var tables = document.querySelectorAll("article table.sortable")
-  tables.forEach(function(table) {
-    new Tablesort(table)
-  })
-})
